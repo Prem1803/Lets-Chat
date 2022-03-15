cd99b11b4c5ae2fa92e12a6333a8ce07ab4f3fbb
Prem3081 <prem.kumar@stepswatch.com>
2022-03-15 14:01:31 +0530

    Server setup and models

---
+++ server/server.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const authRoutes = require("./routes/authRoutes");
+const friendInvitationRoutes = require("./routes/friendInvitationRoutes");
+
+const { createSocketServer } = require("./socket/socketServer");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// register the routes
+app.use("/api/auth", authRoutes);
+app.use("/api/invite-friend", friendInvitationRoutes);
+
+const server = http.createServer(app);
+
+// socket connection
+createSocketServer(server);
+
+const DB_NAME =
+  process.env.NODE_ENV === "production"
+    ? process.env.DB_NAME
+    : process.env.DB_NAME_DEV;
+console.log(DB_NAME);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@lets-chat.tnurt.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server started on Port ${PORT}.....`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed. Server not started");
+    console.error(err);
+  });
