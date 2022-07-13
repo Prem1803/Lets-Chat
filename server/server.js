0ab2a59ee05096095d6fca132e057111bafc9345
Prem3081 <prem.kumar@stepswatch.com>
2022-07-13 15:41:09 +0530

    Heroku Deployment

--- server/server.js
+++ server/server.js
@@ -19,6 +19,17 @@ app.use(cors());
 // register the routes
 app.use("/api/auth", authRoutes);
 app.use("/api/invite-friend", friendInvitationRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/../client/build")));
+
+  // register the routes
+  app.use("/api/auth", authRoutes);
+  app.use("/api/invite-friend", friendInvitationRoutes);
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/../client/build/index.html"));
+  });
+}
 
 const server = http.createServer(app);
 
